88852e5e753096c563e183d94db44a71e117e8b6
Mikhail Mendoza <mikhail.mendoza@infor.com>
2018-06-25 12:26:04 +0800

    for gcp deployment

--- src/core/post-functions/webhook.js
+++ src/core/post-functions/webhook.js
@@ -1,42 +1,39 @@
 var forge = require('node-forge');
 var url = require('url');
-
-var logger = require('../../../config/logger-config').Logger;
 var validator = require('../../utils/validation');
 
 var webhook = function (req, res) {
 
-    const newLocal = "X-Classmarker-Hmac-Sha256"; 
+    const newLocal = "X-Classmarker-Hmac-Sha256";
 
     var headerHmacSignature = req.get(newLocal);
     var jsonData = req.body;
     // You are given a un‌iquе sеc‌ret code when crеati‌ng a Wеbho‌ok. declare in environment variable
     var secret = 'OuKtbnh9nm2Bev7';
 
-    var verified = verifyData(jsonData,headerHmacSignature,secret);
-
-     
     var verified = verifyData(jsonData, headerHmacSignature, secret);
 
     if (verified) {
         // Sa‌vе rеsu‌lts in your databasе.
         // Important: Do not use a script that will take a long timе to respond.
-          // Notify ClassMarker you have recеiv‌ed the Wеbh‌ook.
+        // Notify ClassMarker you have recеiv‌ed the Wеbh‌ook.
         res.sendStatus(200);
     }
     else {
         res.sendStatus(500)
     }
-    console.log(res);
-    logger.info(`Response: StatusCode:${res.statusCode} StatusMessage:${res.statusMessage}`)
-    logger.info(`End Access for: ${url.parse(req.url).pathname}`);
-    logger.info('===============================================');
+    console.log(`Response: StatusCode :${res.statusCode} StatusMessage:${res.statusMessage}`)
+    console.log(`End Access for: ${url.parse(req.url).pathname}`);
+    console.log('===============================================');
 }
 
 var verifyData = function (jsonData, headerHmacSignature, secret) {
+    console.log(`JSON DATA:${JSON.stringify(jsonData)}`);
+    console.log(`HEADER HMAC SIGNATURE:${headerHmacSignature}`);
+    console.log(`SECRET:${secret}`);
     var jsonHmac = computeHmac(jsonData, secret);
+    console.log(`JSON HMAC: ${jsonHmac}`)
     return jsonHmac == headerHmacSignature;
-    // return jsonHmac !== headerHmacSignature;
 };
 
 var computeHmac = function (jsonData, secret) {
