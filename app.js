8d29094f918ada844e39bbd5862836436f260692
Mikhail Mendoza <mikhail.mendoza@infor.com>
2018-07-06 22:18:37 +0800

    test

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ const express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
   forge = require('node-forge'),
-  request = require('request');
+  request = require('request'),
 
+  url = require('url'),
+  _ = require('lodash');
+
+var lms = require('./src/core/soapClient');
+var convertToXml = require('./src/utils/convertToXml');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
+var controller = require('./src/controller/api-controller');
 
-var routes = require('./src/routes/router');
 
-const API_VERSION_PATH = '/v1';
-app.use(API_VERSION_PATH, routes)
+
+app.get('/start', (req, res) => {
+  res.json('Connected Successfully');
+});
+app.post('/webhook', controller.webhook);
+app.get('/launchLmsTest', controller.launchLmsTest);
+app.post('/launchLmsTest', controller.launchLmsTest);
+app.post('/updateUserTest', controller.launchLmsTest);
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
